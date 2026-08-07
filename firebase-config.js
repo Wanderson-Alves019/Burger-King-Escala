48ef5cf41c9d916d4f84fd98b9182699c5d0c710
Wanderson Alves <125202799+Wanderson-Alves019@users.noreply.github.com>
2026-08-07 19:15:52 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -48,7 +48,7 @@ const lojas = [
     {
         id: 2,
         nome: "Águas Lindas Shopping",
-        senha: "Bkb@21920",
+        senha: 'Bkb@21920',
         icone: "assets/lojas/aguaslindasshopping.png",
         cidade: "Águas Lindas - GO"
     },
